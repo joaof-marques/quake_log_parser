ebe69f342f3eef2f5e39e6535afd437480a23580
João Marques <joaofmarques.dev@gmail.com>
2024-07-03 11:39:47 -0300

    Adds rust project structure

---
+++ quake_log_parser/target/debug/deps/quake_log_parser-08291f8fbb6450a1.d
@@ -0,0 +1,5 @@
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/libquake_log_parser-08291f8fbb6450a1.rmeta: src/main.rs
+
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-08291f8fbb6450a1.d: src/main.rs
+
+src/main.rs:
---
+++ quake_log_parser/target/debug/deps/quake_log_parser-24317ceba026705a.d
@@ -0,0 +1,5 @@
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/libquake_log_parser-24317ceba026705a.rmeta: src/main.rs
+
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-24317ceba026705a.d: src/main.rs
+
+src/main.rs:
