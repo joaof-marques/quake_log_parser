9048700d8be518b42dcfc8bcd648893be44f1ad2
João Marques <joaofmarques.dev@gmail.com>
2024-07-04 10:52:50 -0300

    Current main. No updates

--- quake_log_parser/src/main.rs
+++ quake_log_parser/src/main.rs
@@ -12,13 +12,10 @@ use games::Game;
 fn main() {
 
     let mut matches_control: HashMap<&str, Game> = HashMap::new();
-
-    let file_path = "qgames.log";
-    
     let mut current_match = Game::new();
 
+    let file_path = "qgames.log"; 
     let mut result: Vec<Vec<&str>> = Vec::new();
-
     let content = fs::read_to_string(file_path).expect("Failed to read the file");
     
     for line in content.lines(){
