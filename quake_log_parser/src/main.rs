ab5ab33639aaefbf40985d4b0d8047a6b0764d8c
João Marques <joaofmarques.dev@gmail.com>
2024-07-03 22:46:32 -0300

    Adds the main loop to the log reading into main.rs

--- quake_log_parser/src/main.rs
+++ quake_log_parser/src/main.rs
@@ -1,3 +1,35 @@
+use std::fs;
+use std::collections::HashMap;
+use std::thread::current;
+
+mod actions;
+mod games;
+
+use actions::action_handler;
+use games::Game;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut matches_control: HashMap<&str, Game> = HashMap::new();
+
+    let file_path = "qgames.log";
+    
+    let mut current_match = Game::new();
+
+    let mut result: Vec<Vec<&str>> = Vec::new();
+
+    let content = fs::read_to_string(file_path).expect("Failed to read the file");
+    
+    for line in content.lines(){
+        let mut words: Vec<&str> = line.trim().split(' ').collect();
+        words[1] = words[1].trim_end_matches(":");
+        result.push(words);
+    }
+    
+
+    for log in result{
+        continue;
+    }
 }
+
