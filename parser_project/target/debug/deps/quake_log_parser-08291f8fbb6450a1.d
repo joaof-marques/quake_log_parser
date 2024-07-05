a5d341565ba991771fc2f1fe25b37fa6e53780a3
João Marques <joaofmarques.dev@gmail.com>
2024-07-05 11:10:10 -0300

    Updates gitignore to not track target

--- parser_project/target/debug/deps/quake_log_parser-08291f8fbb6450a1.d
+++
@@ -1,9 +0,0 @@
-/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/libquake_log_parser-08291f8fbb6450a1.rmeta: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
-
-/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-08291f8fbb6450a1.d: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
-
-src/main.rs:
-src/utils/mod.rs:
-src/utils/utils.rs:
-src/games/mod.rs:
-src/games/game_struct.rs:
--- parser_project/target/debug/deps/quake_log_parser-24317ceba026705a.d
+++
@@ -1,9 +0,0 @@
-/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/libquake_log_parser-24317ceba026705a.rmeta: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
-
-/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-24317ceba026705a.d: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
-
-src/main.rs:
-src/utils/mod.rs:
-src/utils/utils.rs:
-src/games/mod.rs:
-src/games/game_struct.rs:
