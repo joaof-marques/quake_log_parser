b76f222ee7b7c8f4797e9d6c61e4751e912d57dc
João Marques <joaofmarques.dev@gmail.com>
2024-07-05 00:18:34 -0300

    Changes base folder name

---
+++ parser_project/target/debug/deps/quake_log_parser-ee6628aa678300da.d
@@ -0,0 +1,9 @@
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-ee6628aa678300da: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
+
+/home/joao/cw_challenges/quake_log_parser/quake_log_parser/target/debug/deps/quake_log_parser-ee6628aa678300da.d: src/main.rs src/utils/mod.rs src/utils/utils.rs src/games/mod.rs src/games/game_struct.rs
+
+src/main.rs:
+src/utils/mod.rs:
+src/utils/utils.rs:
+src/games/mod.rs:
+src/games/game_struct.rs:
