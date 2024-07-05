2ac270dd9809f2f8596178db539a0a24b90208a2
João Marques <joaofmarques.dev@gmail.com>
2024-07-04 23:09:55 -0300

    Finishes implementing increase_player_kills and increase kills_by_means methods

--- quake_log_parser/src/games/game_struct.rs
+++ quake_log_parser/src/games/game_struct.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 pub struct Game<'a> {
-    total_kills: u32,
+    pub total_kills: u32,
     pub players: HashMap<u32, &'a str>,
-    pub kills: HashMap<&'a str, u32>,
-    kill_by_means: HashMap<&'a str, u32>,
+    pub kills: HashMap<&'a str, i32>,
+    pub kill_by_means: HashMap<String, u32>,
 }
 
 impl<'a> Game<'a> {
@@ -25,23 +25,45 @@ impl<'a> Game<'a> {
         self.total_kills += 1;
     }
 
-    pub fn insert_update_player_in_match(&mut self, player_name: &'a str, player_id: u32) {
-        self.players.insert(player_id, player_name);
-    }
-
-    pub fn increase_player_kills(&mut self, player_name: &'a str) {
-        if let Some(player_kills) = self.kills.get_mut(&player_name) {
-            *player_kills += 1;
+    pub fn insert_update_player_in_match(&mut self, player_name: &'a str, player_id: &'a str) {
+        if let Ok(parsed_id) = player_id.parse::<u32>() {
+            self.players.insert(parsed_id, player_name);
         } else {
-            self.kills.insert(player_name, 1);
+            // Opcional: trate o erro de conversão aqui
+            eprintln!("Failed to parse player_id: {}", player_id);
         }
     }
 
-    pub fn increase_kill_by_means(&mut self, player_name: &'a str) {
-        if let Some(kill_method) = self.kill_by_means.get_mut(&player_name) {
-            *kill_method += 1;
+    pub fn increase_player_kills(&mut self, killer_id: u32, dead_id: u32) {      
+        
+        if killer_id == 1022 {
+            let deads_name = self
+                .players
+                .get(&dead_id)
+                .expect("Failed to retrieve Dead's name.");
+            if let Some(player_kills) = self.kills.get_mut(*deads_name) {
+                *player_kills -= 1;
+            } else {
+                self.kills.insert(deads_name, -1);
+            }
         } else {
-            self.kills.insert(player_name, 1);
+            let killer_name = self
+            .players
+            .get(&killer_id)
+            .expect("Failed to retrieve Killer Name.");
+            if let Some(player_kills) = self.kills.get_mut(*killer_name) {
+                *player_kills += 1;
+            } else {
+                self.kills.insert(killer_name, 1);
+            }
+        }
+    }
+
+    pub fn increase_kill_by_means(&mut self, kill_method: String) {
+        if let Some(weapon_kill_count) = self.kill_by_means.get_mut(&kill_method) {
+            *weapon_kill_count += 1;
+        } else {
+            self.kill_by_means.insert(kill_method, 1);
         }
     }
 }
